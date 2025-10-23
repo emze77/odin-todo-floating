38a6c30388ce9d12ff6758e50027bb47eef10572
emze <kontakt@marcus-sperber.de>
2025-10-23 04:18:25 +0200

    build: form data is saved

--- src/js/dom.js
+++ src/js/dom.js
@@ -89,6 +89,10 @@ export function openCardDialog (element) {
 
   renderDialogFrame();
   renderCardDetails(element);
+  
+  console.log("step 1")
+
+  createConfirmEvent(element);
 
   dialog.showModal();
 }
@@ -97,6 +101,40 @@ dialog.addEventListener('close', () => {
   renderMainInput();
   renderCards();
 })
+
+function createConfirmEvent(element) {
+  const dialogConfirmButton = document.querySelector("#dialog__confirm-button");
+
+  console.table(element)
+  // console.log("trying to edit this el: " + element)
+
+  dialogConfirmButton.addEventListener("click", () => {
+    const dialogDescriptionValue = document.querySelector("#dialog__description-input").value;
+    const dueDateValue = document.querySelector("#dialog__dueDate-input").value;
+    const checkedRadio = document.querySelector('input[name="prio"]:checked').value;
+
+    console.log("check Radio: " + checkedRadio);
+
+    element.priority = checkedRadio;
+    element.description = dialogDescriptionValue;
+    element.dueDate = dueDateValue;
+  })
+
+
+}
+
+
+
+
+
+// dialog.addEventListener("click", (event) => {
+//     // form should not submit
+//     event.preventDefault();
+//     // returns input when closing
+//     createNewProject([projectDialog.nameInput.value, projectDialog.colorInput.value])
+//     projectDialog.dialog.close();
+//   });
+
 
 function clearDialog () {
  while (dialog.hasChildNodes()) {
@@ -111,17 +149,17 @@ function renderDialogFrame() {
 
   createAndAppend("div", "dialog", "buttons", "", "", "", "dialog__form");
   createAndAppend("button", "dialog", "cancel-button", "dialog__button", {value: "cancel", formmethod: "dialog"}, "Cancel", "dialog__buttons");
-  createAndAppend("button", "dialog", "confirm-buton", "dialog__button", {value: "default", formmethod: "dialog"}, "Okay", "dialog__buttons");
+  createAndAppend("button", "dialog", "confirm-button", "dialog__button", {value: "default", formmethod: "dialog"}, "Okay", "dialog__buttons");
 }
 
 function renderCardDetails (card) {
   createAndAppend("h2", "dialog", "title", "", "", card.title, "dialog__content");
 
   createAndAppend("label", "dialog", "description-label", "dialog__label", {for: "dialog__description-input"}, "Description:", "dialog__content");
-  createAndAppend("textarea", "dialog", "description-input", "dialog__input", { rows: 4, name: "description-input"}, card.description, "dialog__description-label"); 
+  createAndAppend("textarea", "dialog", "description-input", "dialog__input", { rows: 4, name: "description-input",}, card.description, "dialog__description-label"); 
   
   createAndAppend("label", "dialog", "dueDate-label", "dialog__label", {for: "dialog__dueDate-input"}, "Due Date:", "dialog__content");
-  createAndAppend("input", "dialog", "dueDate-input", "dialog__input", {type: "date", name: "dueDate-input", value: "2025-05-05",}, "", "dialog__dueDate-label");
+  createAndAppend("input", "dialog", "dueDate-input", "dialog__input", {type: "date", name: "dueDate-input", value: card.dueDate,}, "", "dialog__dueDate-label");
 
   createAndAppend("fieldset", "dialog", "priority-fieldset", "dialog__fieldset", "", "", "dialog__content");
   createAndAppend("legend", "dialog", "priority-legend", "", "", "Priority:", "dialog__priority-fieldset");
@@ -129,19 +167,34 @@ function renderCardDetails (card) {
   createAndAppend("span", "dialog", "span-prio-high", "", "", "", "dialog__priority-fieldset");
   createAndAppend("input", "dialog", "radio-button-prio-high", "dialog__radio-button", {type: "radio", name: "prio", value: "high"}, "", "dialog__span-prio-high");
   createAndAppend("label", "dialog", "radio-label-prio-high", "dialog__radio-label", {for: "dialog__radio-button-prio-high"}, "High", "dialog__span-prio-high");
-
+  
   createAndAppend("span", "dialog", "span-prio-mid", "", "", "", "dialog__priority-fieldset");
-  createAndAppend("input", "dialog", "radio-button-prio-mid", "dialog__radio-button", {type: "radio", name: "prio", value: "mid"}, "", "dialog__span-prio-mid");
+  createAndAppend("input", "dialog", "radio-button-prio-mid", "dialog__radio-button", {type: "radio", name: "prio", value: "mid",}, "", "dialog__span-prio-mid");
   createAndAppend("label", "dialog", "radio-label-prio-mid", "dialog__radio-label", {for: "dialog__radio-button-prio-mid"}, "Medium", "dialog__span-prio-mid");
-
+  
   createAndAppend("span", "dialog", "span-prio-low", "", "", "", "dialog__priority-fieldset");
   createAndAppend("input", "dialog", "radio-button-prio-low", "dialog__radio-button", {type: "radio", name: "prio", value: "low"}, "", "dialog__span-prio-low");
   createAndAppend("label", "dialog", "radio-label-prio-low", "dialog__radio-label", {for: "dialog__radio-button-prio-low"}, "Low", "dialog__span-prio-low");
+  
+  checkCorrectPrio(card);
+}
 
+function checkCorrectPrio (card) {
+  const highRadioButton = document.querySelector("#dialog__radio-button-prio-high");
+  const midRadioButton = document.querySelector("#dialog__radio-button-prio-mid");
+  const lowRadioButton = document.querySelector("#dialog__radio-button-prio-low");
+
+  if (card.priority === "high") {
+    highRadioButton.setAttribute("checked", true);
+  } else if (card.priority === "low") {
+    lowRadioButton.setAttribute("checked", true);
+  } else {
+    midRadioButton.setAttribute("checked", true);
   }
+}
 
 
-
+// checked = true
 
 
 
