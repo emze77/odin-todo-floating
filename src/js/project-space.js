f3910c3251b10a38cd746145a2e03ecbc5994761
emze <kontakt@marcus-sperber.de>
2025-11-06 20:12:03 +0100

    chore: moving project-button-stylings to styling.js

--- src/js/project-space.js
+++ src/js/project-space.js
@@ -12,6 +12,7 @@ import {
 } from "./dom.js";
 import { rebuildSite } from "./index.js";
 import { saveToLocalStorage } from "./utils.js";
+import {vibrant, nextThemeHint, highlightCurrentProject} from "./styling.js";
 
 export const allProjects = [];
 
@@ -68,7 +69,7 @@ export function renderProjects() {
   createDomAddProjectButton();
 
   for (let i = 0; i < allProjects.length + 1; i++) {
-    styleProjectButtons.vibrant(i);
+    vibrant(i);
   }
 
   themeAdjustmentMainInput();
@@ -145,85 +146,3 @@ export function deleteCurrentProject(project) {
 
 
 
-
-
-// || Style Project Buttons
-
-
-const styleProjectButtons = (function () {
-  const BOTTOM_COLOR_VALUE = 5;
-  const TOP_COLOR_VALUE = 7;
-  let reverse = false;
-  let colorValue = parseInt((BOTTOM_COLOR_VALUE + TOP_COLOR_VALUE) / 2);
-
-  // vibrant: coloring project-buttons in different tones of current color-scheme
-  const vibrant = (iterator) => {
-    const currentProjectMenu = document.querySelectorAll(
-      `.main-block__project-button`
-    );
-
-    // highlight current project
-    if (iterator === 0) {
-      currentProjectMenu[
-        iterator
-      ].style.borderColor = `var(--${currentProject.theme}-5)`;
-      currentProjectMenu[
-        iterator
-      ].style.backgroundColor = `var(--${currentProject.theme}-3)`;
-      currentProjectMenu[
-        iterator
-      ].style.color = `var(--${currentProject.theme}-9)`;
-      currentProjectMenu[
-        iterator
-      ].style.boxShadow = `inset 2px 2px 6px var(--${currentProject.theme}-8)`;
-    } else {
-      currentProjectMenu[
-        iterator
-      ].style.borderColor = `var(--${currentProject.theme}-${colorValue})`;
-      currentProjectMenu[iterator].style.backgroundColor = `var(--${
-        currentProject.theme
-      }-${colorValue + 2})`;
-      currentProjectMenu[iterator].style.color = `var(--${
-        currentProject.theme
-      }-${colorValue - 2})`;
-    }
-
-    // colorValue vibrates between TOP and BOTTOM
-    if (!reverse) {
-      colorValue++;
-      if (colorValue === TOP_COLOR_VALUE) reverse = true;
-    } else {
-      colorValue--;
-      if (colorValue === BOTTOM_COLOR_VALUE) reverse = false;
-    }
-  };
-
-  // show the scheme color of a project while hovering over the button (not in use)
-  const nextThemeHint = (projectData, index) => {
-    const currentProjectMenu = document.querySelector(
-      `#main-block__project-button-${index}`
-    );
-
-    const currentProjectBG = currentProjectMenu.style.backgroundColor;
-
-    currentProjectMenu.addEventListener("mouseover", () => {
-      currentProjectMenu.style.backgroundColor = `var(--${allProjects[index].theme}-7)`;
-    });
-
-    currentProjectMenu.addEventListener("mouseout", function () {
-      currentProjectMenu.style.backgroundColor = currentProjectBG;
-    });
-  };
-
-  const highlightCurrentProject = () => {
-    const currentProjectMenu = document.querySelectorAll(
-      `.main-block__project-button`
-    );
-
-    currentProjectMenu[0].style.borderColor = `var(--${themeColors[0]}-8)`;
-    currentProjectMenu[0].style.backgroundColor = `var(--${themeColors[0]}-6)`;
-    currentProjectMenu[0].style.color = `var(--${themeColors[0]}-2)`;
-  };
-
-  return { vibrant, nextThemeHint, highlightCurrentProject };
-})();
---
+++ src/js/styling.js
@@ -0,0 +1,55 @@
+
+  import { currentProject, themeColors } from "./project-space";
+
+  
+  
+  const BOTTOM_COLOR_VALUE = 5;
+  const TOP_COLOR_VALUE = 7;
+  let reverse = false;
+  let colorValue = parseInt((BOTTOM_COLOR_VALUE + TOP_COLOR_VALUE) / 2);
+
+  // vibrant: coloring project-buttons in different tones of current color-scheme
+  export function vibrant (iterator) {
+    const currentProjectMenu = document.querySelectorAll(
+      `.main-block__project-button`
+    );
+
+    // highlight current project
+    if (iterator === 0) {
+      currentProjectMenu[
+        iterator
+      ].style.borderColor = `var(--${currentProject.theme}-5)`;
+      currentProjectMenu[
+        iterator
+      ].style.backgroundColor = `var(--${currentProject.theme}-3)`;
+      currentProjectMenu[
+        iterator
+      ].style.color = `var(--${currentProject.theme}-9)`;
+      currentProjectMenu[
+        iterator
+      ].style.boxShadow = `inset 2px 2px 6px var(--${currentProject.theme}-8)`;
+    } else {
+      currentProjectMenu[
+        iterator
+      ].style.borderColor = `var(--${currentProject.theme}-${colorValue})`;
+      currentProjectMenu[iterator].style.backgroundColor = `var(--${
+        currentProject.theme
+      }-${colorValue + 2})`;
+      currentProjectMenu[iterator].style.color = `var(--${
+        currentProject.theme
+      }-${colorValue - 2})`;
+    }
+
+    // colorValue vibrates between TOP and BOTTOM
+    if (!reverse) {
+      colorValue++;
+      if (colorValue === TOP_COLOR_VALUE) reverse = true;
+    } else {
+      colorValue--;
+      if (colorValue === BOTTOM_COLOR_VALUE) reverse = false;
+    }
+  };
+
+
+
+  
